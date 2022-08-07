5c5c4fcd1747a9636b845831247963a22b4c3578
Lamar Burke <lamar.burke@mymotorhub.com>
2022-08-07 21:45:20 +0100

    axios is working

--- server/server.js
+++ server/server.js
@@ -1,29 +1,44 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import {PrismaClient} from '@prisma/client';
 
 dotenv.config();
 
 const app = express();
+
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//     })
+// );
+
 const prisma = new PrismaClient();
 
-app.get('/posts', async (req, res, next) => {
-    const posts = await prisma.post.findMany({
-        select: {id: true, title: true},
-    });
+app.get(
+    '/posts',
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    }),
+    async (req, res, next) => {
+        const posts = await prisma.post.findMany({
+            select: {id: true, title: true},
+        });
 
-    // try {
-    //     const posts = await prisma.post.findMany({
-    //         select: {id: true, title: true},
-    //     });
-    //     res.send(posts);
-    // } catch (err) {
-    //     console.log(err);
-    //     next(err);
-    // }
+        // try {
+        //     const posts = await prisma.post.findMany({
+        //         select: {id: true, title: true},
+        //     });
+        //     res.send(posts);
+        // } catch (err) {
+        //     console.log(err);
+        //     next(err);
+        // }
 
-    res.send(posts);
-});
+        res.send(posts);
+    }
+);
 
 async function commitToDb(promise) {
     app.to;
